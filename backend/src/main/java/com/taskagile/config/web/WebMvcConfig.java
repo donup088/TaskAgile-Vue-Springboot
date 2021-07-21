a0351961b0bdc41ad20559e24d0733bacdccd190
donup3 <donup08@naver.com>
2021-07-21 17:57:11 +0900

    logback 설정 test

--- backend/src/main/java/com/taskagile/config/web/WebMvcConfig.java
+++ backend/src/main/java/com/taskagile/config/web/WebMvcConfig.java
@@ -1,15 +1,20 @@
 package com.taskagile.config.web;
 
+import com.taskagile.interceptor.LogInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final LogInterceptor logInterceptor;
     /****************************************************************************************
      * CORS 설정
      * 모두 허용함
@@ -33,5 +38,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
         return characterEncodingFilter;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/css/**")
+                .excludePathPatterns("/js/**")
+                .excludePathPatterns("/index.html");
     }
 }
